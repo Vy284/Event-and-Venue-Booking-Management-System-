fde6eac4f14521848973332648bffdd618377f78
Bloomdang <bloomdang05@gmail.com>
2026-09-23 02:18:42 +0700

    ViewTest

--- EventVenueBooking/Controllers/HomeController.cs
+++ EventVenueBooking/Controllers/HomeController.cs
@@ -8,23 +8,9 @@ namespace EventVenueBooking.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Dashboard()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View("~/Views/Admin/Dashboard.cshtml");
         }
     }
 }
