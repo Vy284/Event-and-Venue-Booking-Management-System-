a20de1698364e218b51150170b00949b89843266
Stick <StDg.ocproject@gmail.com>
2026-09-22 23:50:02 +0700

    Add client ViewModel

---
+++ EventVenueBooking/ViewModels/BookingHistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventVenueBooking.ViewModels
+{
+    public class BookingHistoryViewModel
+    {
+            public int BookingId { get; set; }
+            public string VenueName { get; set; }
+            public string EventTypeName { get; set; }
+            public DateTime EventStartDateTime { get; set; }
+            public DateTime EventEndDateTime { get; set; }
+            public decimal TotalCost { get; set; }
+
+            // Status: 0=Pending, 1=Confirmed, 2=InProgress, 3=Completed, 4=Cancelled
+            public byte Status { get; set; }
+            public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ EventVenueBooking/ViewModels/FeedbackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventVenueBooking.ViewModels
+{
+    public class FeedbackViewModel
+    {
+        public int BookingId { get; set; }
+        public string VenueName { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Vui lòng chọn từ 1 đến 5 sao")]
+        public byte Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
